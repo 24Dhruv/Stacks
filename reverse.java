81152f9970b58e7c9afa63bde429f29f7eec1e30
Dhruv Khandelwal <137927657+24Dhruv@users.noreply.github.com>
2024-07-03 23:46:02 +0530

    Create reverse.java

---
+++ reverse.java
@@ -0,0 +1,11 @@
+public static void main(String args[]) {
+    Stack<Integer> stack = new Stack<>();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+
+    reverse(stack);
+    while(!stack.isEmpty()) {
+        System.out.println(stack.pop());
+    }
+}
